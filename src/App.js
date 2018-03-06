6382e3754ae4e27e5f97759bfb545faf392e9781
farahmed@gmail.com <farahmed@gmail.com>
2018-03-05 21:46:08 -0600

    commiting files after fixing git

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import './App.css';
+ import ToDo from './components/ToDo.js';
+
+class App extends Component {
+  constructor(props) {
+   super(props);
+   this.state = {
+  todos: [
+    { description: 'Make dinner', isCompleted: true },
+    { description: 'Help kids with homework', isCompleted: false },
+    { description: 'Work on Bloc', isCompleted: false }
+  ],
+    newTodoDescription: ''
+};
+ }
+
+ handleChange(e) {
+    this.setState({ newTodoDescription: e.target.value })
+  }
+
+ handleSubmit(e) {
+   e.preventDefault();
+   if (!this.state.newTodoDescription) { return }
+   const newTodo = { description: this.state.newTodoDescription, isCompleted: false };
+   this.setState({ todos: [...this.state.todos, newTodo], newTodoDescription: '' });    
+ }
+
+ toggleComplete(index) {
+   const todos = this.state.todos.slice();
+   const todo = todos[index];
+   todo.isCompleted = todo.isCompleted ? false : true;
+   this.setState({ todos: todos });
+ }
+
+  render() {
+    return (
+      <div className="App">
+      <ul>
+      { this.state.todos.map( (todo, index) =>
+        <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } />
+          )}
+        </ul>
+         <form onSubmit={ (e) => this.handleSubmit(e) }>
+          <input type="text" value={ this.state.newTodoDescription } onChange={ (e) => this.handleChange(e) } />
+          <input type="submit" />
+        </form>
+      </div>
+    );
+  }
+}
+
+
+export default App;
